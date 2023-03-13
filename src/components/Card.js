8bd56ddeddd33228a750ba0e13683df25ac8701a
Elena Zotova <elena.ztv.disk@yandex.ru>
2023-03-13 17:50:08 +0300

    refactor: cards component and show cards

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-function Card() {
+function Card(card) {
     return (
-      <div></div>
+      <article className="places__card">
+        <div className="button places__delete-button"> </div>
+        <img className="button places__image" style={{ backgroundImage: `url(${card.src})` }}/>
+        <div className="places__item">
+          <h2 className="places__name">{card.name}</h2>
+          <div className="places__like-item">
+            <button type="button" aria-label="Добавить в избранное" className="button places__like-button"></button>
+            <div className="places__like-counter">{card.likesCount}</div>
+          </div>
+        </div>
+      </article>
     );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,39 @@
 import React from 'react';
 import api from '../utils/api';
+import Card from './Card';
 
 function Main(props) {
-  const [userName, setUserName] = React.useState();
-  const [userDescription, setUserDescription] = React.useState();
-  const [userAvatar, setUserAvatar] = React.useState();
+  const [userName, setUserName] = React.useState('');
+  const [userDescription, setUserDescription] = React.useState('');
+  const [userAvatar, setUserAvatar] = React.useState('');
+
+  const [cards, setCards] = React.useState([]);
 
   React.useEffect(() => {
-    Promise.all([
-      api.getUser(),
-      api.getInitialCards()
-    ])
-      .then((result) => {
-        const userData = result[0];
-        const cardsData = result[1];
-        console.log(userData);
+    api.getUser()
+      .then((userData) => {
         setUserName(userData.name);
         setUserDescription(userData.about);
         setUserAvatar(userData.avatar);
-
-        // cardList = new Section({
-        //   items: cardsData,
-        //   renderer: (element) => {
-        //     cardList.addItem(generateCard(element), false);
-        //   }
-        // }, places);
-        // cardList.renderItems();
       })
       .catch((err) => {
         console.log(err);
       })
-  })
+
+    api.getInitialCards()
+      .then((cardsData) => {
+        setCards(cardsData.map((item) => ({
+          id: item._id,
+          name: item.name,
+          src: item.link,
+          likesCount: item.likes.length
+        })))
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
+  }, [])
 
   const avatarStyle = {
     backgroundImage: `url(${userAvatar})`,
@@ -58,19 +61,9 @@ function Main(props) {
       </section>
 
       <section className="places">
-        <template id="card-template">
-          <article className="places__card">
-            <div className="button places__delete-button"> </div>
-            <img className="button places__image" />
-            <div className="places__item">
-              <h2 className="places__name"></h2>
-              <div className="places__like-item">
-                <button type="button" aria-label="Добавить в избранное" className="button places__like-button"></button>
-                <div className="places__like-counter"></div>
-              </div>
-            </div>
-          </article>
-        </template>
+        {cards.map((card) =>
+          <Card key={card.id} name={card.name} src={card.src} likesCount={card.likesCount} />
+        )}
       </section>
     </main>
   );
